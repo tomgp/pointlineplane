40cd23c92eefe46ebaf99e410222c2e270836c17
Tom Pearson <tompearson@Toms-MacBook-Air.local>
2012-10-23 20:02:25 +0100

    adding  awhole bunch of assets and centering everything

--- live/js/homepage.js
+++ live/js/homepage.js
@@ -8,27 +8,52 @@ $(function(){
 	//functions
 
 	function prepend_more_link(index, item){
-		var collection = $(item);
-		var story_id = collection.parent().attr('id');
-		collection.before("<a href='#"+story_id+"' id='link_" + story_id + "' class='more_button'>LINK</a>");
-		$('#link_'+story_id).on('click', more_link_clicked);
+		var more_text = $(item);
+		var story_id = more_text.parent().attr('id');
+		console.log();
+		more_text.parent().children('.story_intro').children('p').last().append("<a href='#"+story_id+"' id='link_" + story_id + "' class='more_button'>READ MORE</a>");
+		
+		$('#link_'+story_id).on('click', more_selected).on('tap', more_selected);
 	}
 
-	function more_link_clicked(e){
+	function more_selected(ev){
 		//check the class to see what to do
-		var link = $(e.target);
-		link.parent().children('.story_more').toggleClass('story_more_hidden');
-		link.toggleClass('more_button');
-		link.toggleClass('less_button');
-
-/*		if(link.hasClass('more_button')){
-			link.parent().children('.story_more').removeClass('story_more_hidden');
-			link.removeClass('more_button').addClass('less_button');
-		}else if(link.hasClass('less_button')){
-			link.parent().children('.story_more').addClass('story_more_hidden');
-			link.addClass('more_button').removeClass('less_button');
-		}*/
-
+		var link = $(ev.target);
+		link.parent().parent().parent().children('.story_more').toggleClass('story_more_hidden');
+		link.toggleClass('more_button').toggleClass('less_button');
 		return false;
 	}
 });
+
+//zepto scroll
+// http://maxdegterev.name/experiments/zscroll/static/zepto.scroll.js
+;(function($) {
+	var interpolate = function (source, target, shift) { 
+		return (source + (target - source) * shift); 
+	};
+
+	var easing = function (pos) { 
+	    return (-Math.cos(pos * Math.PI) / 2) + .5; 
+	};
+
+	$.scroll = function(endY, duration, easingF) {
+		endY = endY || ($.os.android ? 1 : 0);
+		duration = duration || 200;
+		(typeof easingF === 'function') && (easing = easingF);
+
+		var startY = window.pageYOffset,
+			startT  = Date.now(),
+			finishT = startT + duration;
+
+		var animate = function() {
+			var now = +(new Date()),
+				shift = (now > finishT) ? 1 : (now - startT) / duration;
+
+			window.scrollTo(0, interpolate(startY, endY, easing(shift)));
+
+			(now > finishT) || setTimeout(animate, 15);
+		};
+
+		animate();
+	};
+}(Zepto));
